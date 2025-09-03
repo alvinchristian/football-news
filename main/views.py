aede54ca38c24033c25d393078ed546a70e51e50
alvinchristian <alvch6@gmail.com>
2025-09-03 13:01:09 +0700

    Update data

--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '240123456',
-        'name': 'Haru Urara',
-        'class': 'PBP A'
+        'npm' : '2406400070',
+        'name': 'Alvin Christian Halim',
+        'class': 'PBP F'
     }
 
     return render(request, "main.html", context)
